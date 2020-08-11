b721ec245f02cbc4a10e10eda8c14f15650618c3
Kinark <igormarcossi@gmail.com>
2020-08-11 16:16:03 -0300

    Added firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: '#',
-  authDomain: '#',
-  databaseURL: '#',
-  projectId: '#',
-  storageBucket: '#',
-  messagingSenderId: '#',
-  appId: '#',
+  apiKey: 'AIzaSyBUSfnaIUCuMD6Jfu_nSH4uk6QZpcBUFyg',
+  authDomain: 'gatsby-blog-b971c.firebaseapp.com',
+  databaseURL: 'https://gatsby-blog-b971c.firebaseio.com',
+  projectId: 'gatsby-blog-b971c',
+  storageBucket: 'gatsby-blog-b971c.appspot.com',
+  messagingSenderId: '63707613419',
+  appId: '1:63707613419:web:761005108d320fc63096ae',
 };
